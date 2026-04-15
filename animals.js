dc7f2fb531e8722da860396e58c52825f2a8ad2b
pitien207 <nghiapham.contact@gmail.com>
2026-04-15 22:18:30 +0200

    Refactor code structure for improved readability and maintainability

--- animals.js
+++ animals.js
@@ -2,105 +2,105 @@ const animalsOriginal = Object.freeze([
   {
     id: "dog",
     name: "Hund",
-    image: "images/dog.svg",
+    image: "images/dog.jpg",
     question:
       "Ich bin der beste Freund des Menschen, belle gern und apportiere Stoecke.",
   },
   {
     id: "cat",
     name: "Katze",
-    image: "images/cat.svg",
+    image: "images/cat.jpg",
     question:
       "Ich schlafe gern in der Sonne, schnurre zufrieden und jage kleine Dinge.",
   },
   {
     id: "elephant",
     name: "Elefant",
-    image: "images/elephant.svg",
+    image: "images/elephant.jpg",
     question:
       "Ich bin sehr gross, habe einen langen Ruessel und vergesse fast nie etwas.",
   },
   {
     id: "bear",
     name: "Baer",
-    image: "images/bear.svg",
+    image: "images/bear.jpg",
     question:
       "Ich bin stark, mag Honig und halte im Winter manchmal einen langen Schlaf.",
   },
   {
     id: "lion",
     name: "Loewe",
-    image: "images/lion.svg",
+    image: "images/lion.jpg",
     question:
       "Ich werde oft Koenig der Tiere genannt und bruellen kann man weit hoeren.",
   },
   {
     id: "tiger",
     name: "Tiger",
-    image: "images/tiger.svg",
+    image: "images/tiger.jpg",
     question:
       "Ich bin eine grosse Wildkatze mit orangefarbenem Fell und dunklen Streifen.",
   },
   {
     id: "monkey",
     name: "Affe",
-    image: "images/monkey.svg",
+    image: "images/monkey.jpg",
     question:
       "Ich klettere gern, bin sehr neugierig und esse am liebsten Bananen.",
   },
   {
     id: "mouse",
     name: "Maus",
-    image: "images/mouse.svg",
+    image: "images/mouse.jpg",
     question:
       "Ich bin klein, flink und verstecke mich schnell, wenn eine Katze kommt.",
   },
   {
     id: "rabbit",
     name: "Kaninchen",
-    image: "images/rabbit.svg",
+    image: "images/rabbit.jpg",
     question:
       "Ich habe lange Ohren, huepfe schnell und knabbere gern an Karotten.",
   },
   {
     id: "fox",
     name: "Fuchs",
-    image: "images/fox.svg",
+    image: "images/fox.jpg",
     question:
       "Ich habe rotbraunes Fell, einen buschigen Schwanz und gelte als schlau.",
   },
   {
     id: "giraffe",
     name: "Giraffe",
-    image: "images/giraffe.svg",
+    image: "images/giraffe.jpg",
     question:
       "Ich habe einen sehr langen Hals, lebe in der Savanne und esse gern Blaetter von hohen Baeumen.",
   },
   {
     id: "penguin",
     name: "Pinguin",
-    image: "images/penguin.svg",
+    image: "images/penguin.jpg",
     question:
       "Ich kann nicht fliegen, watschele ueber Eis und schwimme dafuer umso besser.",
   },
   {
     id: "frog",
     name: "Frosch",
-    image: "images/frog.svg",
+    image: "images/frog.jpg",
     question:
       "Ich quake laut, springe weit und fuehle mich im Wasser und an Land wohl.",
   },
   {
     id: "owl",
     name: "Eule",
-    image: "images/owl.svg",
+    image: "images/owl.jpg",
     question:
       "Ich bin nachts unterwegs, habe grosse Augen und kann meinen Kopf weit drehen.",
   },
   {
     id: "dolphin",
     name: "Delfin",
-    image: "images/dolphin.svg",
+    image: "images/dolphin.jpg",
     question:
       "Ich lebe im Meer, bin sehr klug und springe oft elegant aus dem Wasser.",
   },
